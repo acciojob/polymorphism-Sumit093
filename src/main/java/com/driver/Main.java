bfd06f3ca9d2eb20af9dbae4d58d91fd7fdde4fc
Sumit Haral <146067244+Sumit093@users.noreply.github.com>
2025-11-26 19:45:58 +0530

    Initial commit
    Fix formatting and restore product method functionality

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,24 +2,24 @@ package com.driver;
 
 public class Main {
   public static class Product{
-            public int product(int x, int y) {
-                return x+y;
-            }
-            public int product(int x, int y, int z) {
-                return x+y+z;
-            }
-            public double product(double x, double y) {
-                return x+y;
-            }
-        }
-        public static void main(String[] args){
-            Product p = new Product();
-            int result = p.product(10,20);
-            int result1 = p.product(10,20,30);
-            double result2 = p.product(10.0,20.0);
+    public int product(int x, int y) {
+      return x+y;
+    }
+    public int product(int x, int y, int z) {
+      return x+y+z;
+    }
+    public double product(double x, double y) {
+      return x+y;
+    }
+  }
+  public static void main(String[] args){
+    Product p = new Product();
+    int result = p.product(10,20);
+    int result1 = p.product(10,20,30);
+    double result2 = p.product(10.0,20.0);
 
-            System.out.println(result);
-            System.out.println(result1);
-            System.out.println(result2);
-        }
+    System.out.println(result);
+    System.out.println(result1);
+    System.out.println(result2);
+  }
 }
